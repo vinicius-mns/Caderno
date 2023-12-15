d3b30b2dd8556af8bcf1b4b6203504f449266c0a
vinicius silva <vinicius_dev@hotmail.com>
2023-12-15 19:38:42 -0300

    feat(api/local/Card.ts): Definir Schema para entidade Card
    Usar zod para definir contrato da entidade Card e exportar sua interface
    	modified:   src/api/local/Card.ts

--- src/api/local/Card.ts
+++ src/api/local/Card.ts
@@ -1,7 +1,13 @@
-export interface ICard {
-  
-}
+import type { BaseApi } from './BaseApi'
+import { z } from 'zod'
 
-export class Card {
+const cardSchema = z.object({
+  id: z.string(),
+  content: z.string().min(3),
+  date: z.date(),
+  tags: z.array(z.string())
+})
 
-}
+export type ICard = z.infer<typeof cardSchema>
+
+export class Card implements BaseApi<ICard> {}
