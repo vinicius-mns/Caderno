8e013e19e64efd33b9e4792f631a390e3b73bca2
vinicius silva <vinicius_dev@hotmail.com>
2023-12-16 16:41:09 -0300

    test(api/local/tests/Card.test.ts): Criar pequeno mock de cards 	modified:   src/api/local/tests/Card.test.ts

--- src/api/local/tests/Card.test.ts
+++ src/api/local/tests/Card.test.ts
@@ -1,4 +1,17 @@
 import { describe, test, expect, beforeEach } from 'vitest'
+import { Card, type ICard } from '../Card'
+
+const mockCards = {
+  sucess: {
+    one: { id: '0', content: 'first', date: new Date(), tags: [] },
+    two: { id: '1', content: 'second', date: new Date(), tags: [] },
+    three: { id: '3', content: 'third', date: new Date(), tags: [] }
+  },
+  fail: {
+    contentNumber: { id: '0', content: 'first', date: new Date(), tags: [] },
+    contentEmpty: { id: '0', content: 'second', date: new Date(), tags: [] }
+  }
+}
 
 describe('Card', () => {
 })
