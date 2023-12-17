06da6547f2f5b1df02e575055fde0d7bfca15e5e
vinicius silva <vinicius_dev@hotmail.com>
2023-12-17 18:12:38 -0300

    test(api/local/tests/Tag.test.ts): [Create] Falha com sucesso 	modified:   src/api/local/tests/Tag.test.ts

--- src/api/local/tests/Tag.test.ts
+++ src/api/local/tests/Tag.test.ts
@@ -21,5 +21,28 @@ describe('Tags', () => {
       expect(tags[0]).toBe('first')
       expect(tags[1]).toBe('second')
     })
+
+    test('Falha com sucesso', () => {
+      const tag = new Tags()
+
+      const createMin = tag.create('a')
+      const createMax = tag.create('asdfghjklasdfghjklsdfgjklwertyui')
+      const createNumber = tag.create(2 as unknown as string)
+
+      expect(createMin.status).toBe(400)
+      expect(createMin.data).toBe(
+        '{"issues":[{"code":"too_small","minimum":3,"type":"string","inclusive":true,"exact":false,"message":"String must contain at least 3 character(s)","path":[]}],"name":"ZodError"}'
+      )
+
+      expect(createMax.status).toBe(400)
+      expect(createMax.data).toBe(
+        '{"issues":[{"code":"too_big","maximum":25,"type":"string","inclusive":true,"exact":false,"message":"String must contain at most 25 character(s)","path":[]}],"name":"ZodError"}'
+      )
+
+      expect(createNumber.status).toBe(400)
+      expect(createNumber.data).toBe(
+        '{"issues":[{"code":"invalid_type","expected":"string","received":"number","path":[],"message":"Expected string, received number"}],"name":"ZodError"}'
+      )
+    })
   })
 })
