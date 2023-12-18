fd11f95102537b7d2fc960287b997140c171c22d
vinicius silva <vinicius_dev@hotmail.com>
2023-12-18 12:02:07 -0300

    feat(api/local/Api.ts): [_validationSchema] Adicionar metodo privado 	modified:   src/api/local/Api.ts

--- src/api/local/Api.ts
+++ src/api/local/Api.ts
@@ -7,4 +7,14 @@ export class Api<T> implements BaseApi<T> {
     private _idGenetate = uuidv4,
     private _response = response
   ) {}
+
+  /**
+   * Verifica se as regras de negocio correspondem
+   * @param ent entitdade a ser veficada
+   */
+  private _validationSchema(ent: T) {
+    const verify = this._schema.safeParse(ent)
+
+    if (!verify.success) throw new Error(JSON.stringify(verify.error))
+  }
 }
