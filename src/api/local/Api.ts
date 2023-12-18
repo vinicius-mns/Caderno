ba0859c3511fec99c61789f93c3298ff10cf9fab
vinicius silva <vinicius_dev@hotmail.com>
2023-12-18 12:06:57 -0300

    feat(api/local/Api.ts): [_addOrInsertInLocalStorage ] Adicionar metodo privado 	modified:   src/api/local/Api.ts

--- src/api/local/Api.ts
+++ src/api/local/Api.ts
@@ -17,4 +17,23 @@ export class Api<T> implements BaseApi<T> {
 
     if (!verify.success) throw new Error(JSON.stringify(verify.error))
   }
+
+  /**
+   * Cria uma entidade caso nao exista ou insira caso exista
+   * @param ent entidade a ser criada ou adicionada
+   */
+  private _addOrInsertInLocalStorage(ent: T) {
+    const storage = localStorage.getItem(this.key)
+
+    if (!storage) {
+      const entity = JSON.stringify([ent])
+
+      localStorage.setItem(this.key, entity)
+    } else {
+      const atualEntity = JSON.parse(storage)
+      const entity = JSON.stringify([...atualEntity, ent])
+
+      localStorage.setItem(this.key, entity)
+    }
+  }
 }
