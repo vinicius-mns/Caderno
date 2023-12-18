35c47ab950afe256b036a4ec975b7d56696bb410
vinicius silva <vinicius_dev@hotmail.com>
2023-12-18 12:21:13 -0300

    feat(api/local/Api.ts): Adicionar JsDoc para contructor 	modified:   src/api/local/Api.ts

--- src/api/local/Api.ts
+++ src/api/local/Api.ts
@@ -1,11 +1,18 @@
 import type { BaseApi } from './BaseApi'
 
 export class Api<T> implements BaseApi<T> {
+  /**
+   * @constructor
+   * @param {string} key - A chave para a API.
+   * @param {z.AnyZodObject} _schema - O esquema a ser usado.
+   * @param {Function} [_idGenetate=uuidv4] - A função para gerar o ID.
+   * @param {Function} [_response=response] - A função para gerar a resposta.
+   */
   constructor(
     public key: string,
     private _schema: z.AnyZodObject,
-    private _idGenetate = uuidv4,
-    private _response = response
+    private _idGenetate: Function = uuidv4,
+    private _response: Function = response
   ) {}
 
   /**
