455c4347822df55fdaf3396ef4ab3efc9845ddcc
vinicius silva <vinicius_dev@hotmail.com>
2023-12-18 12:00:31 -0300

    feat(api/local/Api.ts): Adicionar contructor
    contrutor que contera todos os parametros necessário para se criar uma nova entidade no localStorage
    	modified:   src/api/local/Api.ts

--- src/api/local/Api.ts
+++ src/api/local/Api.ts
@@ -1,4 +1,10 @@
 import type { BaseApi } from './BaseApi'
 
 export class Api<T> implements BaseApi<T> {
+  constructor(
+    public key: string,
+    private _schema: z.AnyZodObject,
+    private _idGenetate = uuidv4,
+    private _response = response
+  ) {}
 }
