6cfe583ca5552d6eba3b0e673c024af8d6629456
vinicius silva <vinicius_dev@hotmail.com>
2023-12-18 17:01:18 -0300

    test(api/local/tests/Api.test.ts): [Create] Cria varios com sucesso 	modified:   src/api/local/tests/Api.test.ts

--- src/api/local/tests/Api.test.ts
+++ src/api/local/tests/Api.test.ts
@@ -14,5 +14,23 @@ describe('Testando a Api - base', () => {
 
       expect(create.status).toBe(201)
     })
+
+    test('Cria varios com sucesso', () => {
+      const apiExample = myApiExample()
+
+      const create1 = apiExample.create({ content: 'its work1', number: 1 })
+      const create2 = apiExample.create({ content: 'its work2', number: 2 })
+      const create3 = apiExample.create({ content: 'its work3', number: 3 })
+      const create4 = apiExample.create({ content: 'its work4', number: 4 })
+
+      const storage = JSON.parse(localStorage.getItem(apiExample.key)!) as IMyApiExample[]
+
+      expect(create1.status).toBe(201)
+      expect(create2.status).toBe(201)
+      expect(create3.status).toBe(201)
+      expect(create4.status).toBe(201)
+      expect(storage.length).toBe(4)
+      expect(storage[3].content).toBe('its work4')
+    })
   })
 })
