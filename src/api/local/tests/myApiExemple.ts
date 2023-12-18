5ceee443572a529df8625e1ac240a54680646cdf
vinicius silva <vinicius_dev@hotmail.com>
2023-12-18 16:58:26 -0300

    feat(api/local/tests/myApiExemple.ts): Criar entidade para testes
    Entidade exemplo para teste.
    Ao testar essa unica entidade, garante que todas entidade as intancias da Api tambem passaram no teste
    	new file:   src/api/local/tests/myApiExemple.ts

---
+++ src/api/local/tests/myApiExemple.ts
@@ -0,0 +1,13 @@
+import { Api } from '../Api'
+import { z } from 'zod'
+
+const myApiSchemaExample = z.object({
+  id: z.string().optional(),
+  date: z.date().optional(),
+  number: z.number(),
+  content: z.string().min(3)
+})
+
+export type IMyApiExample = z.infer<typeof myApiSchemaExample>
+
+export const myApiExample = () => new Api<IMyApiExample>('my_api_for_test', myApiSchemaExample)
