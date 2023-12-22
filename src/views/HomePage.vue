aba91342fc99244d192d1a9820c5eee7b3ca01ff
vinicius silva <vinicius_dev@hotmail.com>
2023-12-22 14:54:15 -0300

    feat(views/HomePage.vue): Criar HomePage vazia
    Estrura de pagina que sera visivel em todas as rotas
    	modified:   src/views/HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,72 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
 
 <template>
-  <div>
-    <h1>home page</h1>
+  <div class="home-page">
+    <aside>
+      <p>sideBar</p>
+    </aside>
+    <main>
+      <header>
+        <p>topbar</p>
+      </header>
+      <article>
+        <p>pages</p>
+        <RouterView />
+      </article>
+    </main>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.home-page {
+  // medidas
+  width: 100%;
+  height: 100vh;
+  // displa
+  display: flex;
+  // estilo
+  background-color: red;
+
+  $asideWidth: 18;
+
+  & aside {
+    // medidas
+    height: 100%;
+    width: $asideWidth * 1%;
+    // estilo
+    background-color: blue;
+  }
+
+  & main {
+    // medidas
+    height: 100%;
+    width: (100 - $asideWidth) * 1%;
+    overflow: auto;
+    //display
+    display: flex;
+    flex-direction: column;
+    // estilo
+    background-color: aqua;
+
+    $headerHeight: 6;
+
+    & header {
+      // medidas
+      width: 100%;
+      height: 44px;
+      min-height: 44px;
+      // estilo
+      background-color: pink;
+    }
+
+    & article {
+      // medidas
+      width: 100%;
+      // estilo
+      background-color: purple;
+    }
+  }
+}
+</style>
