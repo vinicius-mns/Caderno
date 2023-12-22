4e5aae5592c3a6d22929e36678075aabd58f4bcb
vinicius silva <vinicius_dev@hotmail.com>
2023-12-22 16:06:49 -0300

    feat(components/topBar/TopBarContainer.vue): Criar component container topbar 	new file:   src/components/topBar/TopBarContainer.vue

---
+++ src/components/topBar/TopBarContainer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="topbar-container">
+    <div>Data</div>
+    <div>Criar card</div>
+    <div>Pesquisa</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.topbar-container {
+  // medidas
+  width: 100%;
+  height: 100%;
+  // display
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+</style>
