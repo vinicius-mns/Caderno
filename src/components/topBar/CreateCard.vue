f748cc886f674d067b473146b630f5f98676ef46
vinicius silva <vinicius_dev@hotmail.com>
2023-12-23 19:49:51 -0300

    feat(components/topBar/CreateCard.vue): [topBar] Criar CreateCard 	new file:   src/components/topBar/CreateCard.vue

---
+++ src/components/topBar/CreateCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="create-card">
+    <button>
+      <p>Criar novo card</p>
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.create-card {
+  // medidas
+  width: 100%;
+  height: 100%;
+  // display
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & button {
+    // medidas
+    height: 32px;
+    width: 94%;
+    // estilo
+    background-color: red;
+    border: none;
+    border-radius: 40px;
+  }
+}
+</style>
