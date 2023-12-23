988375bcb94562516895879d569c997343a828d1
vinicius silva <vinicius_dev@hotmail.com>
2023-12-23 19:30:27 -0300

    feat(components/topBar/AtualDate.vue): [topBar] Criar component AtualDate 	new file:   src/components/topBar/AtualDate.vue

---
+++ src/components/topBar/AtualDate.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+const date = ref(new Date())
+
+watchEffect(() => {
+  setInterval(() => {
+    date.value = new Date()
+  }, 1000)
+})
+</script>
+
+<template>
+  <div class="date">
+    <p>{{ date.getHours() }}</p>
+    :
+    <p>{{ date.getMinutes() }}</p>
+    :
+    <p>{{ date.getSeconds() }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.date {
+  // medidas
+  width: 100%;
+  height: 100%;
+  // display
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+</style>
