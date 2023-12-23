7cab5fcca78d9fa753a3acbb55857aaff91e99c3
vinicius silva <vinicius_dev@hotmail.com>
2023-12-23 18:48:19 -0300

    feat(components/sideBar/UserArea.vue):[sideBar] Criar component UserArea 	new file:   src/components/sideBar/UserArea.vue

---
+++ src/components/sideBar/UserArea.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="user-area">
+    <div class="avatar"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user-area {
+  // medidas
+  width: 100%;
+  height: 100%;
+  // display
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  background-color: goldenrod;
+
+  & .avatar {
+    // medidas
+    width: 50px;
+    aspect-ratio: 1;
+    // estilo
+    border-radius: 50%;
+    background-color: red;
+  }
+}
+</style>
