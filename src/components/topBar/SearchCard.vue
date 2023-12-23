c1b0cc47f187a50aedfecc603c16ea656a866956
vinicius silva <vinicius_dev@hotmail.com>
2023-12-23 20:20:59 -0300

    feat(components/topBar/SearchCard.vue): [topBar]: Criar component de pesquisa 	new file:   src/components/topBar/SearchCard.vue

---
+++ src/components/topBar/SearchCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="search">
+    <input type="text" placeholder="Pesquisar" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.search {
+  // medidas
+  width: 100%;
+  height: 100%;
+  // display
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & input {
+    // medidas
+    width: 68%;
+    height: 32px;
+    // estilo
+    border: none;
+    outline: none;
+    background-color: white;
+    border-radius: 40px;
+    box-sizing: border-box;
+    padding-left: 12px;
+    padding-right: 12px;
+  }
+}
+</style>
