288855b1913fb75fc529b9c61526438a177ad391
vinicius silva <vinicius_dev@hotmail.com>
2023-12-23 18:49:51 -0300

    feat(components/sideBar/ButtonAddTag.vue): [sideBar] Criar component ButtonAddTag 	new file:   src/components/sideBar/ButtonAddTag.vue

---
+++ src/components/sideBar/ButtonAddTag.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button-add-tag">
+    <p>Criar uma nova tag</p>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button-add-tag {
+  // medidas
+  height: 30px;
+  width: 90%;
+  // estilo
+  background-color: red;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  border: none;
+  cursor: pointer;
+}
+</style>
