36959d8765ddb6112ae1d9267d9513479661e8bf
vinicius silva <vinicius_dev@hotmail.com>
2023-12-26 19:22:10 -0300

    feat(api/local/Card.ts): Criar api Card 	new file:   src/api/local/Card.ts

---
+++ src/api/local/Card.ts
@@ -0,0 +1,13 @@
+import { Api } from './Api'
+import { z } from 'zod'
+
+const cardSchema = z.object({
+  id: z.string(),
+  date: z.date(),
+  content: z.string().min(3),
+  tags: z.array(z.string())
+})
+
+export type ICard = z.infer<typeof cardSchema>
+
+export const cardLocal = () => new Api<ICard>('card_local', cardSchema)
