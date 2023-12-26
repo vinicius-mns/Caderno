2c6ad2000251dfee2d3fc9bda4c90acd8dec9fde
vinicius silva <vinicius_dev@hotmail.com>
2023-12-26 18:36:56 -0300

    feat(api/local/Tags.ts): Criar api local para tags 	new file:   src/api/local/Tags.ts

---
+++ src/api/local/Tags.ts
@@ -0,0 +1,11 @@
+import { Api } from './Api'
+import { z } from 'zod'
+
+const tagsSchema = z.object({
+  img: z.string(),
+  content: z.string()
+})
+
+type ITag = z.infer<typeof tagsSchema>
+
+export const tagsApi = new Api<ITag>('tags_local', tagsSchema)
