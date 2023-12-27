3470bcfa24e938b270fe7669e5ccdc1e0d7b3b4c
vinicius silva <vinicius_dev@hotmail.com>
2023-12-27 15:36:30 -0300

    feat(router/index.ts): Definir rotas
    - '/' -> home
    - '/' -> redirecionar para '/cards'
    - '/cards' -> renderiza 'home' e 'cards'
    	modified:   src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
+import HomeCards from '@/views/HomeCards.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomePage
+      redirect: '/cards',
+      component: HomePage,
+      children: [
+        {
+          path: 'cards',
+          component: HomeCards
+        }
+      ]
     }
   ]
 })
