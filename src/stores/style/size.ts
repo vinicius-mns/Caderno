fc287235dd92bc0aea54da102c6cad1015ea0203
vinicius silva <vinicius_dev@hotmail.com>
2023-12-28 21:16:38 -0300

    refactor(stores/style/size.ts): Size 	modified:   src/stores/style/size.ts

--- src/stores/style/size.ts
+++ src/stores/style/size.ts
@@ -1,52 +1,49 @@
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
-interface ISize {
-  font: {
-    h1: string
-    h2: string
-    p: string
-  }
-  button: {
-    heigth: {
-      g: string
-      m: string
-      p: string
-    }
-  }
-  screen: 'desktop' | 'tablet' | 'mobile'
+type IDeviceType = 'mobile' | 'tablet' | 'desktop'
+
+interface IDevice {
+  button: number
+  sideBar: string
 }
 
-export const useSize = defineStore('styleSize', () => {
-  const store: ISize = reactive({
-    font: {
-      h1: '',
-      h2: '',
-      p: ''
+interface IDevices {
+  desktop: IDevice
+  tablet: IDevice
+  mobile: IDevice
+}
+
+export const useSize = defineStore('globalSize', () => {
+  const atualDevice = ref<IDeviceType>('mobile')
+
+  const device = computed<IDevice>(() => devices[atualDevice.value])
+
+  const devices: IDevices = reactive({
+    desktop: {
+      button: 32,
+      sideBar: '260px'
     },
-    button: {
-      heigth: {
-        g: '',
-        m: '',
-        p: ''
-      }
+    tablet: {
+      button: 36,
+      sideBar: '210px'
     },
-    screen: 'desktop'
+    mobile: {
+      button: 40,
+      sideBar: '50px'
+    }
   })
 
-  /**
-   * Usar a medida de largura da tela para definir:
-   * store.screen = 'mibile' | 'tablet' | 'desktop'
-   */
-  const defineSizeScreen = () => {
+  const defineDevice = () => {
     const widthScreen = window.innerWidth
-    if (widthScreen < 768) store.screen = 'mobile'
-    if (widthScreen >= 768) store.screen = 'tablet'
-    if (widthScreen >= 1024) store.screen = 'desktop'
+    if (widthScreen < 768) atualDevice.value = 'mobile'
+    if (widthScreen >= 768) atualDevice.value = 'tablet'
+    if (widthScreen >= 1024) atualDevice.value = 'desktop'
   }
 
   return {
-    store,
-    defineSizeScreen
+    device,
+    atualDevice,
+    defineDevice
   }
 })
