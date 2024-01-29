65e3e8f26486379f7bb1cb5a158582fd82dc3b46
vinicius silva <vinicius_dev@hotmail.com>
2024-01-29 13:51:15 -0300

    feat(components/atoms/FixedCard.vue)[atom]: Criar component
    Card usadado para criar cards flutuantes
    	new file:   src/components/atoms/FixedCard.vue

---
+++ src/components/atoms/FixedCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useStyle } from '@/stores/style'
+import { ref } from 'vue'
+
+const { style } = useStyle()
+
+const props = defineProps<{
+  cursorPosition: { x: string; y: string }
+}>()
+
+const x = ref(props.cursorPosition.x)
+const y = ref(props.cursorPosition.y)
+</script>
+
+<template>
+  <div class="float-card" @click.stop>
+    <slot class="slot"></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$buttonSize: v-bind('style.button.size');
+$cardColor: v-bind('style.component.bgColor');
+$buttonColor: v-bind('style.button.bgColor');
+$boxShadow: v-bind('style.boxShadow');
+.float-card {
+  z-index: 1;
+  position: fixed;
+  padding: 10px;
+  left: v-bind(x);
+  top: v-bind(y);
+  border-radius: 8px;
+  background-color: $cardColor;
+  box-shadow: $boxShadow;
+  // @media screen and (max-width: 768px) {
+  //   left: calc(v-bind(x) - 200px);
+  // }
+}
+</style>
