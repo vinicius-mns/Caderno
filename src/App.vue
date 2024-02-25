74c74667d91531c699dd6a6de1c446d6b301c9da
vinicius silva <vinicius_dev@hotmail.com>
2024-02-24 21:28:59 -0300

    feat(/App.vue): Adicionar ao app regras:
    - definir tamanho dos botoes pelo estilo global
    - regras de estilo para markdown
    	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,51 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
 import { RouterView } from 'vue-router'
+import { useStyle } from './stores/style'
+
+const { defineButtonSize, style } = useStyle()
+
+const resizeButton = () => {
+  const screen = window.innerWidth
+  screen < 768 && defineButtonSize('mobile')
+  screen >= 768 && defineButtonSize('desktop')
+}
+
+onMounted(() => {
+  resizeButton()
+  window.addEventListener('resize', resizeButton)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resizeButton)
+})
 </script>
 
 <template>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style lang="scss">
+$especialColor: v-bind('style.component.bgColor');
+$textColor: v-bind('style.button.textColor');
+a,
+strong {
+  background-color: $especialColor;
+  color: $textColor;
+  padding: 3px;
+  border-radius: 4px;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th,
+td {
+  text-align: left;
+  padding: 8px;
+}
+th {
+  background-color: $especialColor;
+  color: $textColor;
+}
+</style>
