d6410d6f1dd13f577cf877b6e1b7ba9a84b31098
vinicius silva <vinicius_dev@hotmail.com>
2024-02-25 06:22:41 -0300

    feat(/components/template/CardsRander.vue)[template] Criar component 	new file:   src/components/template/CardsRander.vue

---
+++ src/components/template/CardsRander.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CardsList from '../organisms/card/CardsList.vue'
+</script>
+
+<template>
+  <div class="cards-container">
+    <CardsList />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.cards-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
