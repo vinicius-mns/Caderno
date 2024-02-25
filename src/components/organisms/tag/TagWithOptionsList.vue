506276e54ba8ecce80eb29cc3c3077640541fec9
vinicius silva <vinicius_dev@hotmail.com>
2024-02-25 06:20:01 -0300

    feat(components/organisms/tag/TagWithOptionsList.vue)[organism] Criar component 	new file:   src/components/organisms/tag/TagWithOptionsList.vue

---
+++ src/components/organisms/tag/TagWithOptionsList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import TagWithOptions from './TagWithOptions.vue'
+import TagsList from './TagsList.vue'
+import type { ITag } from '@/api/local'
+
+const tags = reactive({
+  value: [] as ITag[],
+  setTags: (e: ITag[]) => {
+    tags.value = e
+  }
+})
+</script>
+
+<template>
+  <TagsList @emit-tags="tags.setTags" search-id="search-tags-with-options" class="tags">
+    <TagWithOptions v-for="(tag, i) in tags.value" :key="i" :tag="tag" />
+  </TagsList>
+</template>
+
+<style scoped lang="scss"></style>
