d4cbb3fa8513c73c1bf2fdb216c8c5e0a2675adf
vinicius silva <vinicius_dev@hotmail.com>
2024-03-05 10:02:10 -0300

    feat(components/organisms/card/CardsRange.vue)[organism] Criar component 	new file:   src/components/organisms/card/CardsRange.vue

---
+++ src/components/organisms/card/CardsRange.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ThemeRange from '@/components/atoms/ThemeRange.vue'
+import { useConfig } from '@/stores/config'
+import { reactive } from 'vue'
+
+const { columnsCard } = useConfig()
+
+const columns = reactive({
+  value: columnsCard.value,
+  set: (e: number) => columnsCard.setColumns(e)
+})
+</script>
+
+<template>
+  <ThemeRange :init-value="columns.value" @emit-value="columns.set" class="range" />
+</template>
+
+<style scoped lang="scss">
+.range {
+  width: 50%;
+}
+</style>
