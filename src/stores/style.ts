6770c5b92f2a25f110d9be03ab75e62c504c68c2
vinicius silva <vinicius_dev@hotmail.com>
2024-03-05 20:09:16 -0300

    fix(stores/style.ts): Style 	modified:   src/stores/style.ts

--- src/stores/style.ts
+++ src/stores/style.ts
@@ -3,7 +3,7 @@ import { reactive } from 'vue'
 
 export const useStyle = defineStore('style', () => {
   const style = reactive({
-    boxShadow: '0 0 1px 1px rgba(255, 255, 255, 0.3)',
+    boxShadow: '0 0 0px 0px rgba(255, 255, 255, 0.3)',
     button: {
       bgColor: '#313131',
       hoverColor: '#424242',
@@ -34,14 +34,14 @@ export const useStyle = defineStore('style', () => {
       style.component.bgColor = '#202020'
       style.page.bgColor = '#191919'
       style.button.hoverColor = '#424242'
-      style.boxShadow = '0 0 1px 1px rgba(255, 255, 255, 0.3)'
+      style.boxShadow = '0 0 0px 0px rgba(255, 255, 255, 0.3)'
     } else {
       style.button.textColor = 'black'
       style.button.bgColor = 'white'
-      style.component.bgColor = 'pink'
+      style.component.bgColor = '#B6E0FE'
       style.page.bgColor = '#e6e6e6'
       style.button.hoverColor = '#e6e6e6'
-      style.boxShadow = '0 0 1px 1px rgba(0, 0, 0, 0.2)'
+      style.boxShadow = '0 0 0px 0px rgba(0, 0, 0, 0.2)'
     }
   }
 
