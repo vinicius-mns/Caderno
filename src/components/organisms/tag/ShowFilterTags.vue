9c1f3330a7c876516fca8b4a49a14f61a23d0879
vinicius silva <vinicius_dev@hotmail.com>
2024-03-05 19:06:04 -0300

    feat(components/organisms/tag/ShowFilterTags.vue)[organisms] Criar component

---
+++ src/components/organisms/tag/ShowFilterTags.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import ThemeIco from '../../atoms/ThemeIco.vue'
+import { useConfig } from '@/stores/config'
+
+const config = useConfig()
+
+const filter = reactive({
+  value: config.showFilterCards.value,
+  toggle: () => {
+    config.showFilterCards.setValue()
+    filter.value = config.showFilterCards.value
+  }
+})
+</script>
+
+<template>
+  <ThemeIco
+    ico="📌"
+    content="filtro"
+    size="24px"
+    @click="filter.toggle"
+    class="config"
+    :colored="filter.value"
+  />
+</template>
+
+<style scoped lang="scss"></style>
