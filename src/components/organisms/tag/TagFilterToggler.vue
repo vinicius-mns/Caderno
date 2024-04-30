9e1a26a8ca13203f48d60830b44cb9b2cfe5e4cf
vinicius silva <vinicius_dev@hotmail.com>
2024-04-30 19:11:58 -0300

    feat(components/organisms/tag/TagFilterToggler.vue): Criar component 	new file:   src/components/organisms/tag/TagFilterToggler.vue

---
+++ src/components/organisms/tag/TagFilterToggler.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ThemeActionButton from '@/components/atoms/ThemeActionButton.vue'
+import { computed } from 'vue'
+import { useConfig } from '@/stores/config'
+
+const config = useConfig()
+
+const filter = computed(() => config.filterCardsByTag)
+
+const click = () => config.toggleFilterByTag()
+</script>
+
+<template>
+  <ThemeActionButton
+    @click="click"
+    ico="📌"
+    description="[Activa / desativar] filtro"
+    :class="filter ? 'filter' : ''"
+  />
+</template>
+
+<style scoped lang="scss">
+.filter {
+  box-shadow: 0 0 0 2px rgb(55, 133, 55);
+}
+</style>
