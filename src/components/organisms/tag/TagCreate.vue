5e9cfa27bf52114690acfd76c0561681f2de3178
vinicius silva <vinicius_dev@hotmail.com>
2024-04-30 19:10:16 -0300

    refactor(components/organisms/tag/TagCreate.vue): Implementar ThemeActionButton 	modified:   src/components/organisms/tag/TagCreate.vue

--- src/components/organisms/tag/TagCreate.vue
+++ src/components/organisms/tag/TagCreate.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
-// import CenterModal from '../../atoms/CenterModal.vue'
+import { ref } from 'vue'
 import TagEditor from '../../molecules/TagEditor.vue'
-import ThemeButton from '@/components/atoms/ThemeButton.vue'
 import { useTags } from '@/stores/local/tags'
 import CenterModal from '@/components/molecules/CenterModal.vue'
+import CenterModalSlot from '@/components/molecules/CenterModalSlot.vue'
+import ThemeActionButton from '@/components/atoms/ThemeActionButton.vue'
+import ThemeButton from '@/components/atoms/ThemeButton.vue'
 
 const tag = useTags()
+
+const props = withDefaults(
+  defineProps<{
+    large?: boolean
+  }>(),
+  {
+    large: false
+  }
+)
 
 const initTag = {
   emoji: '',
@@ -23,18 +33,19 @@ const createTag = (e: typeof initTag) => {
 </script>
 
 <template>
-  <CenterModal
-    button-content="Criar nova tag 🏷️"
-    title-modal="Criar nova tag"
-    ref="centerModal"
-    class="button-tag-create"
-  >
-    <TagEditor :tag="initTag" @emit-tag="createTag" />
-  </CenterModal>
+  <CenterModalSlot title-modal="Criar nova tag">
+    <template #button-modal>
+      <ThemeButton v-if="props.large" class="large">Criar nova tag 🏷️</ThemeButton>
+      <ThemeActionButton description="Criar nova tag" ico="🏷️" v-else />
+    </template>
+    <template #center-modal>
+      <TagEditor :tag="initTag" @emit-tag="createTag" />
+    </template>
+  </CenterModalSlot>
 </template>
 
 <style scoped lang="scss">
-.button-tag-create {
+.large {
   width: 100%;
 }
 </style>
