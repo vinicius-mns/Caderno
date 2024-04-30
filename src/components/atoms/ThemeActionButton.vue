7720aff6858502166d873b8667121e3755f52a96
vinicius silva <vinicius_dev@hotmail.com>
2024-04-30 16:59:18 -0300

    feat(ThemeActionButton): Criar component 	new file:   src/components/atoms/ThemeActionButton.vue

---
+++ src/components/atoms/ThemeActionButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ThemeButton from '@/components/atoms/ThemeButton.vue'
+import FloatDescription from '@/components/molecules/FloatDescription.vue'
+
+const props = defineProps<{
+  description: string
+  ico: string
+}>()
+</script>
+
+<template>
+  <FloatDescription :content="props.description">
+    <ThemeButton class="action-button">{{ props.ico }}</ThemeButton>
+  </FloatDescription>
+</template>
+
+<style scoped lang="scss">
+.action-button {
+  width: 40px;
+  height: 40px;
+  flex-shrink: 0;
+  font-size: 24px;
+}
+</style>
