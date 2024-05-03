059d86f17524625c97bc27330878598d35bd28aa
vinicius silva <vinicius_dev@hotmail.com>
2024-05-03 00:33:50 -0300

    feat: Estilo Card prototipo 	modified:   src/components/organisms/card/CardConfigModal.vue 	modified:   src/components/organisms/card/CardView.vue 	new file:   src/components/organisms/config/style/CardsStyleSelector.vue 	new file:   src/stores/cardStyle.ts

---
+++ src/components/organisms/config/style/CardsStyleSelector.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import CardView from '../../card/CardView.vue'
+import type { ICard } from '@/api/api_local/entites/cards/CardsTypes'
+
+import { useCardStyle } from '@/stores/cardStyle'
+import { computed } from 'vue'
+
+const cardStyle = useCardStyle()
+
+const allCardsStyles = computed(() => cardStyle.cardsStyle)
+
+const mockCard: ICard = {
+  content: 'mock card',
+  date: new Date(),
+  id: '0',
+  tags: ['']
+}
+
+const setCard = (index: number) => {
+  cardStyle.setCardStyle(index)
+}
+</script>
+
+<template>
+  <div class="container-card-style-selector">
+    <CardView
+      class="card"
+      :card="mockCard"
+      v-for="(CStyle, i) in allCardsStyles"
+      :key="i"
+      :style="CStyle"
+      @click="() => setCard(i)"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container-card-style-selector {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  & .card {
+    margin: 10px;
+    width: 100%;
+  }
+}
+</style>
+@/stores/cardStyle
---
+++ src/stores/cardStyle.ts
@@ -0,0 +1,57 @@
+import { cardStyleLocalDb } from '@/api/api_local/entites/cardsStyle/Card_Style_api'
+// import type { ICardStyle } from '@/api/api_local/entites/cardsStyle/CardsStyleTypes'
+import { defineStore } from 'pinia'
+import { reactive, ref, watch } from 'vue'
+
+export const useCardStyle = defineStore('cardStyle', () => {
+  const cardStyleApi = cardStyleLocalDb()
+
+  const history = reactive({
+    value: [] as string[]
+  })
+
+  const cardInit = [
+    {
+      color: 'white',
+      border: 'none',
+      background: '#202020',
+      'border-radius': '8px',
+      'box-shadow': 'none'
+    },
+    {
+      color: 'black',
+      border: 'none',
+      background: 'white',
+      'border-radius': '8px',
+      'box-shadow': 'none'
+    },
+    {
+      color: 'black',
+      border: 'none',
+      background:
+        'linear-gradient(38deg, rgba(255,246,161,1) 0%, rgba(255,177,177,1) 77%, rgba(186,211,249,1) 100%)',
+      'border-radius': '8px',
+      'box-shadow': 'rgb(204, 204, 204) 0 0 8px 2px'
+    }
+  ]
+
+  const cardsStyle = ref(cardInit)
+
+  const atualCardSyle = ref(cardInit[0])
+
+  const setCardStyle = (index: number) => {
+    atualCardSyle.value = cardsStyle.value[index]
+  }
+
+  const updateCards = () => {
+    cardsStyle.value = cardStyleApi.read()
+  }
+
+  watch(history, updateCards, { deep: true })
+
+  return {
+    cardsStyle,
+    atualCardSyle,
+    setCardStyle
+  }
+})
