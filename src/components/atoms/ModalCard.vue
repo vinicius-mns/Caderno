25ab9e750224c9198eb12665e9bc1e282ac38fb4
vinicius silva <vinicius_dev@hotmail.com>
2024-07-02 19:21:58 -0300

    fix: Component 	modified:   src/components/atoms/ModalCard.vue

--- src/components/atoms/ModalCard.vue
+++ src/components/atoms/ModalCard.vue
@@ -11,17 +11,11 @@ const { atualStyle } = useStyle()
 </template>
 
 <style scoped lang="scss">
-$borderRadius: v-bind('atualStyle.borderRadius.two');
-$buttonSize: 36px;
-$cardColor: v-bind('atualStyle.color.one');
-$bgColor: v-bind('atualStyle.color.four');
-$boxShadow: v-bind('atualStyle.boxShadow');
 .modal-card-container {
-  padding: 10px;
+  padding: 15px;
   box-sizing: border-box;
-  border-radius: $borderRadius;
-  background-color: $bgColor;
-  box-shadow: $boxShadow;
-  border: v-bind('atualStyle.border');
+  border-radius: v-bind('atualStyle.borderRadius.two');
+  background-color: v-bind('atualStyle.color.two');
+  box-shadow: v-bind('atualStyle.boxShadow');
 }
 </style>
