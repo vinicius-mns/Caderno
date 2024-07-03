cad9cc3c4dcfd785141cd02eab37fc80520c7080
vinicius silva <vinicius_dev@hotmail.com>
2024-07-02 23:06:50 -0300

    fix: TagDelete

--- src/components/organisms/TagDelete.vue
+++ src/components/organisms/TagDelete.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import type { ITag } from '@/api/api_local/entites/tags/TagsTypes'
-import FloatModalSlot from '@/components/atoms/FloatModalSlot.vue'
-import ThemeP from '@/components/atoms/ThemeP.vue'
 import TrashIco from '@/components/atoms/icons/TrashIco.vue'
 import ButtonOption from '@/components/molecules/ButtonOption.vue'
 import TagView from '@/components/molecules/TagView.vue'
@@ -16,31 +14,23 @@ const deletar = () => emit('emitDelete', props.tag)
 </script>
 
 <template>
-  <FloatModalSlot>
-    <template #button-slot>
-      <ButtonOption content="Deletar Tag">
-        <TrashIco />
-      </ButtonOption>
-    </template>
-    <template #container-slot>
-      <div class="container">
-        <ThemeP content="Deseja deletar essa tag?" class="item" />
-        <TagView :tag="props.tag" class="item" />
-        <ButtonOption content="Deletar" @click="deletar" class="item">
-          <TrashIco />
-        </ButtonOption>
-      </div>
-    </template>
-  </FloatModalSlot>
+  <div class="container">
+    <TagView :tag="props.tag" class="item" />
+    <ButtonOption content="Deletar" @click="deletar" class="item">
+      <TrashIco />
+    </ButtonOption>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .container {
-  width: 100%;
-  width: 100%;
+  width: 300px;
+  max-width: 92dvw;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 15px;
+  box-sizing: border-box;
   & .item {
     margin: 3px;
   }
