f284fc0eb1da3059e3ec84318fadf2ed4f75de05
vinicius silva <vinicius_dev@hotmail.com>
2024-07-03 11:03:10 -0300

    feat: Um monte de coisa que criei sem commitar, que sao inuteis agora. 	deleted:    CardConfigModal.vue 	deleted:    TagListSlot.vue 	deleted:    xCardListSlot.vue 	new file:   xCardOneView.vue 	new file:   xCardsList.vue 	new file:   xCardsWithOptions.vue 	new file:   xControlPanel.vue 	new file:   xPanelButton.vue 	new file:   xSideActions.vue 	new file:   xSideContainer.vue 	new file:   xWindowModal.vue
    	modified:   ../../App.vue
    	deleted:    ../template/CardCreateWindow.vue
    	deleted:    ../template/CardsWithOptions.vue
    	deleted:    ../template/FilterCardsWindow.vue
    	deleted:    ../template/MenuWindow.vue
    	deleted:    ../template/TagCreteWindow.vue
    	deleted:    ../template/TagDeleteCardsWindow.vue
    	deleted:    ../template/TagDeleteWindow.vue
    	deleted:    ../template/TagUpdateWindow.vue
    	deleted:    ../template/TagsWindow.vue
    	deleted:    ../../stores/local/about.ts
    	deleted:    ../../stores/local/routes.ts
    	deleted:    ../../stores/stylex.ts

---
+++ src/components/organisms/xSideActions.vue
@@ -0,0 +1,21 @@
+<!-- <script setup lang="ts">
+import FilterIco from '../atoms/icons/FilterIco.vue'
+import MenuIco from '../atoms/icons/MenuIco.vue'
+import PencilIco from '../atoms/icons/PencilIco.vue'
+import TagIco from '../atoms/icons/TagIco.vue'
+import CoinButton from '../molecules/CoinButton.vue'
+</script>
+
+<template>
+  <div class="container-actions">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container-actions {
+  display: flex;
+  justify-content: space-evenly;
+  height: 60px;
+}
+</style> -->
---
+++ src/components/organisms/xSideContainer.vue
@@ -0,0 +1,16 @@
+<!-- <script setup lang="ts"></script>
+
+<template>
+  <div class="side-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.side-container {
+  width: 100%;
+  height: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+}
+</style> -->
