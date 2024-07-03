374ca99318c73a2114e9a9c11b4a8ace6805e4fb
vinicius silva <vinicius_dev@hotmail.com>
2024-07-02 23:00:01 -0300

    feat: xShowPageStyle 	new file:   src/components/molecules/xShowPageStyle.vue

---
+++ src/components/molecules/xShowPageStyle.vue
@@ -0,0 +1,31 @@
+<!-- <script setup lang="ts">
+import { useStyle } from '@/stores/style'
+import ThemeP from '../atoms/ThemeP.vue'
+
+const { optionsStyle } = useStyle()
+</script>
+
+<template>
+  <div class="colors" v-for="(option, i) in optionsStyle" :key="i">
+    <ThemeP content="texto" :style="{ color: option.color.text }" />
+    <div class="color" :style="{ backgroundColor: option.color.one }"></div>
+    <div class="color" :style="{ backgroundColor: option.color.two }"></div>
+    <div class="color" :style="{ backgroundColor: option.color.three }"></div>
+    <div class="color" :style="{ backgroundColor: option.color.four }"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.colors {
+  width: 100%;
+  height: 80px;
+  background-color: rgb(116, 116, 116);
+  display: flex;
+  align-items: center;
+  & .color {
+    margin: 3px;
+    height: 40px;
+    aspect-ratio: 1;
+  }
+}
+</style> -->
