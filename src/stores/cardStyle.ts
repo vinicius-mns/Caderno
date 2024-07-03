7170969bbe4fb0c04e76aff74b0bed556aa2e220
vinicius silva <vinicius_dev@hotmail.com>
2024-07-02 23:18:25 -0300

    esse ta ruim vo arruma dps 	modified:   src/stores/cardStyle.ts

--- src/stores/cardStyle.ts
+++ src/stores/cardStyle.ts
@@ -12,6 +12,7 @@ export const useCardStyle = defineStore('cardStyle', () => {
 
   const cardInit = [
     {
+      name: 'name1',
       color: 'white',
       border: 'none',
       background: '#202020',
@@ -19,6 +20,7 @@ export const useCardStyle = defineStore('cardStyle', () => {
       'box-shadow': 'none'
     },
     {
+      name: 'name2',
       color: 'black',
       border: 'none',
       background: 'white',
@@ -26,12 +28,31 @@ export const useCardStyle = defineStore('cardStyle', () => {
       'box-shadow': 'none'
     },
     {
+      name: 'name3',
       color: 'black',
       border: 'none',
       background:
         'linear-gradient(38deg, rgba(255,246,161,1) 0%, rgba(255,177,177,1) 77%, rgba(186,211,249,1) 100%)',
-      'border-radius': '8px',
-      'box-shadow': 'rgb(204, 204, 204) 0 0 8px 2px'
+      'border-radius': '8px'
+      // 'box-shadow': 'rgb(204, 204, 204) 0 0 8px 2px'
+    },
+    {
+      name: 'name4',
+      color: 'black',
+      border: 'none',
+      'background-image': `url('https://64.media.tumblr.com/fa143cffbc3bfb0183302cdad6aa5238/tumblr_pq229bewd61uzwgsuo1_400.gif')`,
+      'background-size': 'cover',
+      'background-position': 'center',
+      'border-radius': '8px'
+    },
+    {
+      name: 'name5',
+      color: 'black',
+      border: 'none',
+      'background-image': `url('https://i.pinimg.com/originals/40/d0/3e/40d03e2ac7a086def11a7738713c9c31.gif')`,
+      'background-size': 'cover',
+      'background-position': 'center',
+      'border-radius': '8px'
     }
   ]
 
