6f6dc7b1010e758cf1fd1027b2aca5e64ab3fa61
vinicius silva <vinicius_dev@hotmail.com>
2024-07-02 23:05:48 -0300

    feat: Criar component PageConfig

---
+++ src/components/organisms/PageConfig.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ThemeP from '../atoms/ThemeP.vue'
+import OptionContainer from '../molecules/OptionContainer.vue'
+import PageStyleSelector from '../molecules/PageStyleSelector.vue'
+
+const emit = defineEmits<{
+  (e: 'emitStyle', v: number): void
+}>()
+
+const emitStyle = (e: number) => emit('emitStyle', e)
+</script>
+
+<template>
+  <OptionContainer title="Page">
+    <OptionContainer title="Estilo">
+      <PageStyleSelector @emit-style="emitStyle" />
+    </OptionContainer>
+    <OptionContainer title="Medidas">
+      <ThemeP content="nada ainda" />
+    </OptionContainer>
+    <OptionContainer title="Outros">
+      <ThemeP content="nada ainda" />
+    </OptionContainer>
+  </OptionContainer>
+</template>
+
+<style scoped lang="scss"></style>
