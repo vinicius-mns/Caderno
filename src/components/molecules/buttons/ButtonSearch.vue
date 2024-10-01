6e5675275160e8474de51e32d778221fa6f99c6e
vinicius silva <vinicius_dev@hotmail.com>
2024-09-30 23:10:55 -0300

    feat: Criar botao com icone Lupa 	new file:   src/components/molecules/buttons/ButtonSearch.vue

---
+++ src/components/molecules/buttons/ButtonSearch.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import BaseButtonSlot from './BaseButtonSlot.vue'
+import BaseButtonCoinSlot from './BaseButtonCoinSlot.vue'
+import SearchIco from '@/components/atoms/icons/SearchIco.vue'
+
+const props = withDefaults(
+  defineProps<{
+    content?: string
+    minimize?: boolean
+  }>(),
+  {
+    content: 'Procurar',
+    minimize: false
+  }
+)
+</script>
+
+<template>
+  <div>
+    <BaseButtonSlot :content="props.content" v-if="!props.minimize">
+      <SearchIco />
+    </BaseButtonSlot>
+
+    <BaseButtonCoinSlot :description="props.content" v-if="props.minimize">
+      <SearchIco />
+    </BaseButtonCoinSlot>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
