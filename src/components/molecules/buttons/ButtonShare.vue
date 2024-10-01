46a9d46e6eadc0a4c15150a519ee60c94eba0066
vinicius silva <vinicius_dev@hotmail.com>
2024-09-30 23:11:38 -0300

    feat: Criar botao com icone compartilhar 	new file:   src/components/molecules/buttons/ButtonShare.vue

---
+++ src/components/molecules/buttons/ButtonShare.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import BaseButtonSlot from './BaseButtonSlot.vue'
+import BaseButtonCoinSlot from './BaseButtonCoinSlot.vue'
+import ShareIco from '@/components/atoms/icons/ShareIco.vue'
+
+const props = withDefaults(
+  defineProps<{
+    content?: string
+    minimize?: boolean
+  }>(),
+  {
+    content: 'Compartilhar',
+    minimize: false
+  }
+)
+</script>
+
+<template>
+  <div>
+    <BaseButtonSlot :content="props.content" v-if="!props.minimize">
+      <ShareIco />
+    </BaseButtonSlot>
+
+    <BaseButtonCoinSlot :description="props.content" v-if="props.minimize">
+      <ShareIco />
+    </BaseButtonCoinSlot>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
