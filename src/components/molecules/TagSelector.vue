9d6c4b0e7c9177f35b3af2c1e81d2ac8ce165d76
vinicius silva <vinicius_dev@hotmail.com>
2024-10-03 20:27:31 -0300

    refactor: Alterar component
    remove o flotmodal do component para usar-lo como irmao
    	modified:   src/components/molecules/TagSelector.vue

--- src/components/molecules/TagSelector.vue
+++ src/components/molecules/TagSelector.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import type { Itag } from '@/stores/tags/Interfaces'
-import ModalCard from '../atoms/ModalCard.vue'
 import FlexContainer from '../atoms/FlexContainer.vue'
 import CheckBoxBase from '../atoms/CheckBoxBase.vue'
 import TagView from '../molecules/TagView.vue'
+import BaseButtonSlot from './buttons/BaseButtonSlot.vue'
+import CheckIco from '../atoms/icons/CheckIco.vue'
+import ModalCard from '../atoms/ModalCard.vue'
 
 const props = defineProps<{ tagsSelected: Itag[]; allTags: Itag[] }>()
 
@@ -20,6 +22,7 @@ const tagsSelectedAdd = (tag: Itag) => (tagsSelected.value = [...tagsSelected.va
 
 const tagsSelectedRemove = (tag: Itag) => {
   const tags = tagsSelected.value
+
   tagsSelected.value = tags.filter((t) => t[1] !== tag[1])
 }
 
@@ -32,38 +35,76 @@ const addOrRemoveTag = (name: string) => {
     if (tagIsChecked(tag[1])) tagsSelectedRemove(tag)
     else tagsSelectedAdd(tag)
   }
-
-  emit('emitSelected', tagsSelected.value)
 }
+
+const isModify = computed(() => {
+  if (tagsSelected.value.length !== props.tagsSelected.length) return true
+
+  const tagsAtualSort = [...props.tagsSelected].sort()
+
+  const tagsRefSort = [...tagsSelected.value].sort()
+
+  for (let i = 0; i < tagsAtualSort.length; i += 1) {
+    const tagsAtualSortName = tagsAtualSort[i][1]
+
+    const tagsRefSortName = tagsRefSort[i][1]
+
+    if (tagsAtualSortName !== tagsRefSortName) return true
+  }
+
+  return false
+})
+
+const emitSelected = () => emit('emitSelected', tagsSelected.value)
 </script>
 
 <template>
-  <FlexContainer flex-direction="column">
-    <ModalCard class="modal-card">
-      <FlexContainer flex-wrap="wrap" :style="{ 'overflow-x': 'auto' }">
-        <CheckBoxBase
-          v-for="(tag, i) in tagsList"
-          :key="i"
-          :id="tag[1]"
-          :is-checked="tagIsChecked(tag[1])"
-          checkbox-name="select-tag-in-card"
-          @select="addOrRemoveTag"
-          :style="{ width: 'calc(50% - 4px)', margin: '2px' }"
-        >
-          <TagView :tag="tag" />
-        </CheckBoxBase>
-      </FlexContainer>
-    </ModalCard>
-  </FlexContainer>
+  <ModalCard class="modal-card-tags">
+    <FlexContainer flex-wrap="wrap" :style="{ 'overflow-x': 'auto' }" class="tags">
+      <CheckBoxBase
+        v-for="(tag, i) in tagsList"
+        :key="i"
+        :id="tag[1]"
+        :is-checked="tagIsChecked(tag[1])"
+        checkbox-name="select-tag-in-card"
+        @select="addOrRemoveTag"
+        :style="{ width: 'calc(50% - 4px)', margin: '2px' }"
+      >
+        <TagView :tag="tag" />
+      </CheckBoxBase>
+    </FlexContainer>
+
+    <BaseButtonSlot
+      content="Confirmar alteração"
+      :class="[isModify ? '' : 'block', 'confirm-button']"
+      :visible="true"
+      @click="emitSelected()"
+    >
+      <CheckIco />
+    </BaseButtonSlot>
+  </ModalCard>
 </template>
 
 <style scoped lang="scss">
-.modal-card {
-  box-sizing: border-box;
-  width: 350px;
-  max-width: 95dvw;
-  max-height: 50dvh;
+.modal-card-tags {
   display: flex;
   flex-direction: column;
+  width: 350px;
+  max-width: 95dvw;
+  height: 50dvh;
+
+  & .tags {
+    height: 100%;
+    overflow: auto;
+  }
+
+  & .confirm-button {
+    margin-top: 10px;
+  }
+
+  & .block {
+    cursor: not-allowed;
+    opacity: 30%;
+  }
 }
 </style>
