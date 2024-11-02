7ed2b7360f1658778233c4d2542e7ac7f7a967f9
vinicius silva <vinicius_dev@hotmail.com>
2024-11-01 23:19:13 -0300

    feat: Criar component para mensagens flutuantes 	new file:   src/components/molecules/FloatMessage.vue

---
+++ src/components/molecules/FloatMessage.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import ModalCard from '../atoms/ModalCard.vue'
+import ThemeP from '../atoms/ThemeP.vue'
+
+const showMessage = ref(false)
+
+const messageRef = ref('')
+
+const closeMessage = () => (showMessage.value = false)
+
+const openMessage = (message: string) => {
+  messageRef.value = message
+
+  showMessage.value = true
+}
+
+const timeToClose = 1000
+
+watchEffect(() => {
+  if (showMessage.value === true) {
+    setTimeout(closeMessage, timeToClose)
+  }
+})
+
+defineExpose({ openMessage, timeToClose })
+</script>
+
+<template>
+  <ModalCard v-if="showMessage" class="message-container">
+    <ThemeP :content="messageRef" />
+  </ModalCard>
+</template>
+
+<style scoped lang="scss">
+.message-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 90%);
+  opacity: 0;
+  animation: intro 0.3s forwards;
+}
+@keyframes intro {
+  to {
+    opacity: 100%;
+    transform: translate(-50%, 50%);
+  }
+}
+</style>
