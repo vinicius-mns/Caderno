6072367bab66689f4f9e12bb9aa136bf043261db
vinicius silva <vinicius_dev@hotmail.com>
2024-11-11 17:38:16 -0300

    faeat: criar metodo de pesquisa de emoji (pinia) 	modified:   src/stores/emojis.ts

--- src/stores/emojis.ts
+++ src/stores/emojis.ts
@@ -1,12 +1,26 @@
 import { defineStore } from 'pinia'
 import * as emoji from 'node-emoji'
+import { ref } from 'vue'
 
 export const useEmoji = defineStore('emoji', () => {
+  const initialEmojis = () => {
+    return emoji.search('').map(({ emoji }) => emoji)
+  }
+
+  const allEmojis = ref<string[]>(initialEmojis())
+
   const getAll = (name = '') => {
     return emoji.search(name.toLocaleLowerCase()).map(({ emoji }) => emoji)
   }
 
+  const filterEmojiByName = (name: string) => {
+    const emojis = emoji.search(name.toLocaleLowerCase()).map(({ emoji }) => emoji)
+    allEmojis.value = emojis
+  }
+
   return {
+    allEmojis,
+    filterEmojiByName,
     getAll
   }
 })
