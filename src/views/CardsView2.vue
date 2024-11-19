550eabf02dd50c9633f192f2676330ac92b90f66
vinicius silva <vinicius_dev@hotmail.com>
2024-11-19 18:52:35 -0300

    feat: adicionar nova pagina com novo template para cards 	modified:   src/router/index.ts 	new file:   src/views/CardsView2.vue

---
+++ src/views/CardsView2.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import FlexContainer from '@/components/atoms/FlexContainer.vue'
+import CardsMain from '@/components/template/CardsMain.vue'
+import CardsSide from '@/components/template/CardsSide.vue'
+import PageTemplate from '@/components/template/PageTemplate.vue'
+import WindowsAll from '@/components/template/windows/WindowsAll.vue'
+import { useCards } from '@/stores/cards/cards'
+import { useTags } from '@/stores/tags/tags'
+import { onMounted } from 'vue'
+
+const cards = useCards()
+
+const tags = useTags()
+
+onMounted(async () => {
+  await tags.init()
+
+  await cards.atualizeReactiveCards({
+    includeTags: tags.includeTags,
+    excludeTags: tags.excludeTags
+  })
+})
+</script>
+
+<template>
+  <div>
+    <PageTemplate>
+      <template #side>
+        <CardsSide />
+      </template>
+      <template #page>
+        <CardsMain />
+      </template>
+    </PageTemplate>
+
+    <WindowsAll />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
